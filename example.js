da9aa03f6959da6acc75f5657618839789aa1a67
tsq <1530234656@qq.com>
2017-09-08 18:52:44 +0800

    Update

--- example.js
+++ example.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const agent = require("./lib/index");
-app.use(agent({name: 'socket'}));
 
+app.use(agent({name: 'socket'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.all('/ping', (req, res) => {
   res.json({success: true});
 });
